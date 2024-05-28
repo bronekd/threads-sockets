7172b7219dceca703fcf9b6843f9fcb07dffe6e5
Bronislav Dřímal <bronislavdrimal@email.cz>
2024-05-28 19:24:30 +0200

    řešení task bank s lock

--- threads_bank.py
+++ threads_bank.py
@@ -71,3 +71,41 @@ thread2.join()
 
 # Očekávaný výsledek je 500, ale může být odlišný kvůli race condition
 print("Zůstatek na účtu po transakcích 2:", balance)
+
+
+
+# řešení od učitele pomocí with lock:
+
+import threading
+import time
+
+
+balance = 1000
+lock = threading.Lock()
+
+def make_transaction(amount):
+    global balance
+
+    with lock:
+        current_balance = balance
+
+        time.sleep(0.1)
+        balance = current_balance - amount
+
+
+thread1 = threading.Thread(target=make_transaction, args=(200,))
+thread2 = threading.Thread(target=make_transaction, args=(300,))
+
+
+thread1.start()
+thread2.start()
+
+
+thread1.join()
+thread2.join()
+
+# Očekávaný výsledek je 500, ale může být odlišný kvůli race condition
+print("Zůstatek na účtu po transakcích:", balance)
+
+
+

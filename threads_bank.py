de1e24b41f615119b31443a3f83c22e265134073
Bronislav Dřímal <bronislavdrimal@email.cz>
2024-05-28 19:21:09 +0200

    moje řešení lock

--- threads_bank.py
+++ threads_bank.py
@@ -22,6 +22,7 @@ thread2 = threading.Thread(target=make_transaction, args=(300,))
 
 
 thread1.start()
+time.sleep(0.2)
 thread2.start()
 
 
@@ -31,3 +32,42 @@ thread2.join()
 # Očekávaný výsledek je 500, ale může být odlišný kvůli race condition
 print("Zůstatek na účtu po transakcích:", balance)
 
+
+
+#další řešení
+# Globální proměnná sdílená mezi vlákny
+balance = 1000
+
+# Vytvoření Threading Lock
+lock = threading.Lock()
+
+# Funkce reprezentující transakci
+def make_transaction(amount):
+    lock.acquire()
+    try:
+        global balance
+        current_balance = balance
+        # Simulace nějakého výpočtu nebo operace
+        # Mezitím může jiné vlákno změnit hodnotu balance
+        # a tím způsobit race condition
+        time.sleep(0.2)
+
+        balance = current_balance - amount
+    finally:
+        # Uvolnění zámku
+        lock.release()
+
+# Vytvoření dvou vláken pro provedení transakcí
+thread1 = threading.Thread(target=make_transaction, args=(200,))
+thread2 = threading.Thread(target=make_transaction, args=(300,))
+
+
+thread1.start()
+thread2.start()
+
+
+thread1.join()
+thread2.join()
+
+# Očekávaný výsledek je 500, ale může být odlišný kvůli race condition
+print("Zůstatek na účtu po transakcích 2:", balance)

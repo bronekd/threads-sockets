daab6b064e3731f96f0b7707dd37c846e1ab2194
Bronislav Dřímal <bronislavdrimal@email.cz>
2024-06-04 18:28:51 +0200

    start nová apliakce komuniakce se serverem přeposílání zpráv

---
+++ socket_v2/client_2_v2.py
@@ -0,0 +1,22 @@
+import socket
+
+
+def start_client():
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_socket.connect(('localhost', 12347))
+
+    while True:
+        message = input("You: ")
+        client_socket.sendall(message.encode())
+        if message.lower() == 'bye':
+            break
+
+        reply = client_socket.recv(1024).decode()
+        print(f"Server: {reply}")
+        if reply.lower() == 'bye':
+            break
+
+    client_socket.close()
+
+
+start_client()
